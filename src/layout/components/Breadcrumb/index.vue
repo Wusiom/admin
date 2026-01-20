39b12e3af16f01fdf05a6bb7ecd05f4d0d7bee1e
Wusiom <2601395542@qq.com>
2026-01-20 17:33:24 +0800

    feat: refactor Breadcrumb component to use dynamic data and improve navigation

--- src/layout/components/Breadcrumb/index.vue
+++ src/layout/components/Breadcrumb/index.vue
@@ -1,18 +1,27 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <!-- 面包屑的最后一项 -->
-    <el-breadcrumb-item>
-      <span class="no-redirect">活动详情</span>
-    </el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item
+        v-for="(item, index) in breadcrumbData"
+        :key="item.path"
+      >
+        <!-- 不可点击项 -->
+        <span v-if="index === breadcrumbData.length - 1" class="no-redirect">{{
+          item.meta.title
+        }}</span>
+        <!-- 可点击项 -->
+        <a v-else class="redirect" @click.prevent="onLinkClick(item)">{{
+          item.meta.title
+        }}</a>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
 <script setup>
 import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import useUserStore from '@/store/user'
 const route = useRoute()
 // 生成数组数据
 const breadcrumbData = ref([])
@@ -22,6 +31,17 @@ const getBreadcrumbData = () => {
   )
   console.log(breadcrumbData.value)
 }
+// 处理点击事件
+const router = useRouter()
+const onLinkClick = (item) => {
+  console.log(item)
+  router.push(item.path)
+}
+
+// 将来需要进行主题替换，所以这里获取下动态样式
+const store = useUserStore()
+// eslint-disable-next-line
+const linkHoverColor = ref(store.cssVar.menuBg)
 // 监听路由变化时触发
 watch(
   route,
@@ -44,6 +64,15 @@ watch(
   ::v-deep .no-redirect {
     color: #97a8be;
     cursor: text;
+  }
+  .redirect {
+    color: #666;
+    font-weight: 600;
+  }
+
+  .redirect:hover {
+    // 将来需要进行主题替换，所以这里不去写死样式
+    color: v-bind(linkHoverColor);
   }
 }
 </style>
